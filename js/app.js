2413ad9f581c010e989107aa6f86f4ccccec3991
Christopher Collins <chris@spring215.com>
2015-11-06 17:53:20 -0500

    stuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
-
 function expandCreateTaskInterface() {
 	document.getElementById('createTaskInterface').setAttribute('class', 'row create-task-interface');
+	document.getElementById('addTaskIcon').setAttribute('class', 'right rotate');
+	document.getElementById('addTaskIcon').setAttribute('onclick', 'hideCreateTaskInterface()');
 
 	initClearInputs()
 }
@@ -15,15 +16,25 @@ function initClearInputs() {
 
 function addTask() {
 
-	var taskName = document.getElementById('taskName').value;
+	var taskName = document.getElementById('taskName').value; // Grabs the UL container
 
-	var container = document.createElement('ui');
-	var node = document.createElement('li');                 // Create a <li> node
-	var textNode = document.createTextNode(taskName); 
+	var node = document.createElement('li'); // Creates a <li> node
+	var taskNameContainer = document.createElement('span'); // Creates a <span> node for the Name of the task
+		taskNameContainer.setAttribute('class', 'task-name')
 
-	container.appendChild(node);        // Create a text node
-	node.appendChild(textNode);                              // Append the text to <li>
-	document.getElementById("taskList").appendChild(container);     // Append <li> to <ul> with id="myList"
+	var textNode = document.createTextNode(taskName); // Creates a text node		
+
+	taskNameContainer.appendChild(textNode);
+	node.appendChild(taskNameContainer);                              // Append the text to <li>
+	document.getElementById('taskList').appendChild(node);     // Append <li> to <ul> with id="myList"
+
+	var listLength = document.getElementById('taskList').getElementsByTagName('li'); // Counts number of task items in list
+	var position = listLength.length;
+	node.setAttribute('name', position);
+
+	var identifier = Math.floor((Math.random() * 10000) + 1);
+	node.setAttribute('id', identifier);                   // Append the text to <li>
+	document.getElementById('taskList').appendChild(node);     // Append <li> to <ul> with id="myList"
 	
 	clearInputs()
 }
@@ -38,5 +49,7 @@ function clearInputs() {
 
 function hideCreateTaskInterface() {
 	document.getElementById('createTaskInterface').setAttribute('class', 'row hide create-task-interface');
+	document.getElementById('addTaskIcon').setAttribute('class', 'right');
+	document.getElementById('addTaskIcon').setAttribute('onclick', 'expandCreateTaskInterface()');
 }
 
