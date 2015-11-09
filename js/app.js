a1c905b37805379fa37f861cf561773d2718179d
Christopher Collins <chris@spring215.com>
2015-11-09 17:21:53 -0500

    java adds

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
-
+// Expands the task creation interface and clears any values that we left from a previous task add
 function expandCreateTaskInterface() {
 	document.getElementById('createTaskInterface').setAttribute('class', 'row create-task-interface');
 	document.getElementById('addTaskIcon').setAttribute('class', 'right rotate');
 	document.getElementById('addTaskIcon').setAttribute('onclick', 'hideCreateTaskInterface()');
-
 	initClearInputs()
 }
 
 function initClearInputs() {
 	document.getElementById('taskName').value = ' ';
-
-
-	//hideCreateTaskInterface()
+	document.getElementById('initPriorityOption').selected = true;
 }
+
+function show() {
+	var taskPriority = document.getElementById('id="taskPriority"');
+	taskPriority.setAttribute('class', ' ');
+};
 
 function addTask() {
 
+	
+
 	var taskName = document.getElementById('taskName').value; // Grabs the UL container
+
 
 	var node = document.createElement('li'); // Creates a <li> node
 	var taskNameContainer = document.createElement('span'); // Creates a <span> node for the Name of the task
@@ -36,20 +41,43 @@ function addTask() {
 	node.setAttribute('id', identifier);                   // Append the text to <li>
 	document.getElementById('taskList').appendChild(node);     // Append <li> to <ul> with id="myList"
 	
-	clearInputs()
+	hideCreateTaskInterface()
+	hideTaskPriority()
+	hideAddButton()
+};
+
+// Enables the task priority options after the task name has been entered
+function showTaskPriority() {
+	var taskPriority = document.getElementById('taskPriority');
+	taskPriority.setAttribute('class', ' ');
+};
+
+// Enables the add button options after the priority ranking has been set
+function showAddButton() {
+	var addButton = document.getElementById('addButton');
+	addButton.setAttribute('class', 'tiny radius right add-task-button')
+	addButton.disabled = false;
 }
 
-function clearInputs() {
-	document.getElementById('taskName').value = ' ';
-
-
-	//hideCreateTaskInterface()
+// Returns input values to the 'disabled state' after the add task window closes
+function hideTaskPriority() {
+	var taskPriority = document.getElementById('taskPriority');
+	taskPriority.setAttribute('class', 'hide');
+};
+// Returns input values to the 'disabled state' after the add task window closes
+function hideAddButton() {
+	var addButton = document.getElementById('addButton');
+	addButton.setAttribute('class', 'tiny radius right add-task-button disabled')
+	addButton.disabled = true;
 }
+
 
 
 function hideCreateTaskInterface() {
 	document.getElementById('createTaskInterface').setAttribute('class', 'row hide create-task-interface');
 	document.getElementById('addTaskIcon').setAttribute('class', 'right');
 	document.getElementById('addTaskIcon').setAttribute('onclick', 'expandCreateTaskInterface()');
+	hideTaskPriority()
+	hideAddButton()
 }
 
